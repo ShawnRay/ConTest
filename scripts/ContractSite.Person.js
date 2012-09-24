96da4f654ac66313fdd205402e2ee54d9fc03012
Shawn Nasiso Ray <shawn.ray.mac@gmail.com>
2012-09-24 18:03:50 +0800

    Add party and person overview; Fix print overview;

---
+++ scripts/ContractSite.Person.js
@@ -0,0 +1,628 @@
+﻿function detailInit(e) {
+    var detailRow = e.detailRow;
+    var availableTags = [{
+        text: "Zorgverzekeraar",
+        value: "1"
+    }, {
+        text: "Zorgaanbieder",
+        value: "2"
+    }, {
+        text: "Patient",
+        value: "3"
+    }, {
+        text: "Tandarts",
+        value: "4"
+    }, {
+        text: "Ziekenhuis",
+        value: "5"
+    }, {
+        text: "Huisarts",
+        value: "6"
+    }, {
+        text: "Toevoegen",
+        value: "7"
+    }];
+
+    detailRow.find(".tabstrip").kendoTabStrip({
+        animation: {
+            open: {
+                effects: "fadeIn"
+            }
+        }
+    });
+
+    $('#divContractDetails input:text, input:password').button().css({
+        'font': 'inherit',
+        'color': 'inherit',
+        'text-align': 'left',
+        'outline': 'none',
+        'cursor': 'text'
+    });
+
+    $(".inputField").qtip({
+        content: {
+            attr: "title"
+        },
+        style: {
+            classes: 'ui-tooltip-dark ui-tooltip-shadow ui-tooltip-rounded'
+        },
+        position: {
+            my: 'left top'
+            //corner : {
+            //	arget : 'rightMiddle',
+            //	tooltip : 'leftMiddle'
+            //}
+        },
+        show: {
+            when: {
+                event: 'focus'
+            }
+        },
+        hide: {
+            when: {
+                event: 'blur'
+            }
+        }
+    });
+
+    $(".imgNotification").qtip({
+        content: "Click it set notification",
+        style: {
+            classes: 'ui-tooltip ui-tooltip-shadow ui-tooltip-rounded'
+        },
+        position: {
+            my: 'left top'
+            //corner : {
+            //	arget : 'rightMiddle',
+            //	tooltip : 'leftMiddle'
+            //}
+        },
+        show: {
+            when: {
+                event: 'focus'
+            }
+        },
+        hide: {
+            when: {
+                event: 'blur'
+            }
+        }
+    });
+
+    $(".imgNotification").click(function () {
+        $("#divSetNotification").dialog('open');
+    });
+
+    $("#divSetNotification").dialog({
+        title: 'Notification',
+        autoOpen: false,
+        height: 200,
+        width: 600,
+        modal: true,
+        buttons: {
+            "Save": function () {
+                $.gritter.add({
+                    // (string | mandatory) the heading of the notification
+                    title: 'Notification Center',
+                    // (string | mandatory) the text inside the notification
+                    text: 'New notification have been set.',
+                    // (string | optional) the image to display on the left
+                    image: 'images/White_circle/alarm.png',
+                    // (bool | optional) if you want it to fade out on its own or just sit there
+                    sticky: false,
+                    // (int | optional) the time you want it to be alive for before fading out
+                    time: '3000'
+                });
+
+                return false;
+            },
+            "Return": function () {
+                $("#divSetNotification").dialog('close');
+            }
+        }
+    });
+
+    $(".inputField").hide();
+
+    $("#ddlContractType").width(220).kendoComboBox({
+        dataTextField: "text",
+        dataValueField: "value",
+        filter: "contains",
+        dataSource: availableTags
+    });
+
+    $("#btnEdit").button().click(function () {
+        if ($("#detailEditStatus").val() == "0") {
+            $("#btnEdit span").html('Save');
+            $("#detailEditStatus").val("1");
+
+            $("#btnCancelEdit").show();
+
+            $(".contentField").hide();
+            $(".inputField").show();
+            return;
+        }
+
+        if ($("#detailEditStatus").val() == "1") {
+            $("#btnEdit span").html('Aanpassen');
+            $("#detailEditStatus").val("0");
+
+            $("#btnCancelEdit").hide();
+
+            $(".contentField").show();
+            $(".inputField").hide();
+
+            $.gritter.add({
+                // (string | mandatory) the heading of the notification
+                title: 'Notification Center',
+                // (string | mandatory) the text inside the notification
+                text: 'Contract Algemeen has been updated.',
+                // (string | optional) the image to display on the left
+                image: 'images/White_circle/document_blank.png',
+                // (bool | optional) if you want it to fade out on its own or just sit there
+                sticky: false,
+                // (int | optional) the time you want it to be alive for before fading out
+                time: '3000'
+            });
+
+            return false;
+        }
+
+        if ($(e.item).children(".k-link").html() == $('#menu-images li:last span').html()) {
+
+            $.gritter.add({
+                // (string | mandatory) the heading of the notification
+                title: 'Notification Center',
+                // (string | mandatory) the text inside the notification
+                text: 'Help.',
+                // (string | optional) the image to display on the left
+                image: 'images/White_circle/alarm.png',
+                // (bool | optional) if you want it to fade out on its own or just sit there
+                sticky: false,
+                // (int | optional) the time you want it to be alive for before fading out
+                time: '3000'
+            });
+
+            $("#divHelpContent").dialog('open');
+
+            return false;
+        }
+    });
+
+    $("#btnCancelEdit").button().click(function () {
+        $("#btnEdit span").html('Aanpassen');
+        $("#btnCancelEdit").hide();
+        $("#detailEditStatus").val("0");
+
+        $(".contentField").show();
+        $(".inputField").hide();
+    });
+
+    $('#divContractDetails table').each(function () {
+        $(this).addClass("table table-striped table-hover");
+    });
+
+    var crudServiceBaseUrl = "http://demos.kendoui.com/service", dataSource = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: crudServiceBaseUrl + "/Products",
+                dataType: "jsonp"
+            },
+            update: {
+                url: crudServiceBaseUrl + "/Products/Update",
+                dataType: "jsonp"
+            },
+            destroy: {
+                url: crudServiceBaseUrl + "/Products/Destroy",
+                dataType: "jsonp"
+            },
+            create: {
+                url: crudServiceBaseUrl + "/Products/Create",
+                dataType: "jsonp"
+            },
+            parameterMap: function (options, operation) {
+                if (operation !== "read" && options.models) {
+                    return {
+                        models: kendo.stringify(options.models)
+                    };
+                }
+            }
+        },
+        batch: true,
+        pageSize: 10,
+        schema: {
+            model: {
+                id: "ProductID",
+                fields: {
+                    ProductID: {
+                        editable: false,
+                        nullable: true
+                    },
+                    ProductName: {
+                        validation: {
+                            required: true
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    $("#divContractContact").kendoGrid({
+        dataSource: dataSource,
+        pageable: true,
+        toolbar: ["create"],
+        columns: [{
+            field: "ProductName",
+            title: "Achtemaan"
+        }, {
+            field: "ProductName",
+            title: "Voornaam"
+        }, {
+            field: "ProductName",
+            title: "Functie"
+        }, {
+            command: ["edit", "destroy"],
+            title: "&nbsp;"
+        }],
+        editable: "inline"
+    });
+}
+
+function onOpen(e) {
+    //kendoConsole.log("Opened: " + $(e.item).children(".k-link").text());
+
+}
+
+function onClose(e) {
+    //kendoConsole.log("Closed: " + $(e.item).children(".k-link").text());
+}
+
+function onSelect(e) {
+    if ($(e.item).children(".k-link").text() == 'Home') {
+        //window.location.replace("http://127.0.0.1:8020/KendoUIDemos/Home.html");
+
+        window.location.replace("http://192.168.166.16/ContractDemo/Home.html");
+    }
+
+    if ($(e.item).children(".k-link").text() == 'Bewerk/Bekijk') {
+        //window.location.replace("http://127.0.0.1:8020/KendoUIDemos/OverviewContract.html");
+
+        window.location.replace("http://192.168.166.16/ContractDemo/OverviewContract.html");
+    }
+
+    if ($(e.item).children(".k-link").text() == 'Notificaties') {
+
+        $.gritter.add({
+            // (string | mandatory) the heading of the notification
+            title: 'Notification Center',
+            // (string | mandatory) the text inside the notification
+            text: '5 notifications.',
+            // (string | optional) the image to display on the left
+            image: 'images/White_circle/alarm.png',
+            // (bool | optional) if you want it to fade out on its own or just sit there
+            sticky: false,
+            // (int | optional) the time you want it to be alive for before fading out
+            time: '3000'
+        });
+
+        return false;
+    }
+}
+
+
+$(document).ready(function () {
+
+    $("#divDemoPersonContracsList").kendoGrid({
+        dataSource: {
+            data: createPersonRandomData(50),
+            schema: {
+                model: {
+                    fields: {
+                        LastName: {
+                            type: "string"
+                        },
+                        FirstName: {
+                            type: "string"
+                        },
+                        Nummer: {
+                            type: "string"
+                        }
+                    }
+                }
+            },
+            pageSize: 16
+        },
+        height: 600,
+        detailTemplate: kendo.template($("#template").html()),
+        detailInit: detailInit,
+        dataBound: function () {
+            //this.expandRow(this.tbody.find("tr.k-master-row").first());
+        },
+        editable: "inline",
+        scrollable: true,
+        pageable: true,
+        columns: [{
+            field: "LastName",
+            title: "Achternaam"
+        }, {
+            field: "FirstName",
+            title: "Voornaam"
+        }, {
+            field: "Nummer",
+            title: "Nummer"
+        }, {
+            command: ["edit", "destroy"],
+            title: "&nbsp;"
+        }]
+    });
+
+    $("#divAddress").kendoGrid({
+        dataSource: {
+            data: createAdresRandomData(1),
+            pageSize: 10,
+            schema: {
+                model: {
+                    fields: {
+                        Adres: {
+                            type: "string"
+                        },
+                        Postbus: {
+                            type: "string"
+                        },
+                        Postcode: {
+                            type: "string"
+                        },
+                        Plaats: {
+                            type: "string"
+                        },
+                        Land: {
+                            type: "string"
+                        }
+                    }
+                }
+            }
+        },
+        height: 300,
+        scrollable: true,
+        pageable: true,
+        toolbar: ["create"],
+        editable: "inline",
+        columns: [{
+            field: "Adres",
+            title: "Adres"
+        }, {
+            Field: "Postbus",
+            title: "Postbus"
+        }, {
+            Field: "Postcode",
+            title: "Postcode"
+        }, {
+            Field: "Plaats",
+            title: "Plaats"
+        }, {
+            Field: "Land",
+            title: "Land"
+        }, {
+            command: ["edit", "destroy"],
+            title: "&nbsp;"
+        }]
+    });
+
+    var sexdata = [
+                        { text: " ", value: "1" },
+                        { text: "Man", value: "2" },
+                        { text: "Vrouw", value: "3" }
+                    ];
+
+    $("#Sex").kendoDropDownList({
+        dataTextField: "text",
+        dataValueField: "value",
+        dataSource: sexdata,
+        index: 0
+    });
+
+    var titledata = [
+                        { text: " ", value: "1" },
+                        { text: "Titel1", value: "2" },
+                        { text: "Titel2", value: "3" }
+                    ];
+
+    $("#Titel").kendoDropDownList({
+        dataTextField: "text",
+        dataValueField: "value",
+        dataSource: titledata,
+        index: 0
+    });
+
+    $("#datepicker").kendoDatePicker();
+
+    $("#menu-images").kendoMenu({
+        dataSource: [{
+            text: "Home",
+            imageUrl: "images/Black_circle/home.png"
+        }, {
+            text: "Contracten",
+            imageUrl: "images/Black_circle/document.png",
+            items: [{
+                text: "Nieuw"
+            }, {
+                text: "Bewerk/Bekijk"
+            }]
+        }, {
+            text: "Notificaties",
+            imageUrl: "images/Black_circle/alarm.png"
+        }, {
+            text: "Partij",
+            imageUrl: "images/Black_circle/profile_group.png",
+            items: [{
+                text: "Nieuw"
+            }, {
+                text: "Bewerk/Bekijk"
+            }]
+        }, {
+            text: "Persoon",
+            imageUrl: "images/Black_circle/profile_add.png",
+            items: [{
+                text: "Nieuw"
+            }, {
+                text: "Bewerk/Bekijk"
+            }]
+        }, {
+            text: "Zoek",
+            imageUrl: "images/Black_circle/rect_search.png"
+        }, {
+            text: "Track and Trace",
+            imageUrl: "images/Black_circle/satellite.png"
+        }, {
+            text: "Rapportage",
+            imageUrl: "images/Black_circle/chart.png"
+        }, {
+            text: '',
+            imageUrl: "images/Black_circle/question.png"
+        }],
+        select: onSelect,
+        open: onOpen,
+        close: onClose
+    });
+
+    $('#menu-images li:last').attr('style', 'float: right; padding-left: 8px;');
+
+    $("#divStep2").hide();
+    $("#divStep3").hide();
+    $("#divStep4").hide();
+    $("#stepRecord").val("0");
+
+    $("#labelStepTitle").html(" 1: Algemene informatie");
+
+    $(".imgDeleteConfirm").click(function () {
+        $("#divDeleteConfirmation").dialog('open');
+    });
+
+    $(".imgArchiveConfirm").click(function () {
+        $("#divArchiveConfirmation").dialog('open');
+    });
+
+    $("#divDeleteConfirmation").dialog({
+        title: 'Contract',
+        autoOpen: false,
+        height: 120,
+        width: 460,
+        modal: true
+    });
+
+    $("#divArchiveConfirmation").dialog({
+        title: 'Contract',
+        autoOpen: false,
+        height: 120,
+        width: 460,
+        modal: true
+    });
+
+    $("#dialog-form").dialog({
+        title: 'Niew Partij',
+        autoOpen: false,
+        height: 500,
+        width: 800,
+        modal: true,
+        buttons: {
+            "Terug": function () {
+                if ($("#stepRecord").val() == "1") {
+                    $("#divStep1").show();
+                    $("#divStep2").hide();
+                    $("#divStep3").hide();
+                    $("#divStep4").hide();
+
+                    $("#stepRecord").val("0");
+                    $("#labelStepTitle").html(" 1: Algemene informatie");
+
+                    $(".ui-dialog-buttonset span:eq(1)").html('Volgende');
+                    return;
+                }
+
+                if ($("#stepRecord").val() == "2") {
+                    $("#divStep1").hide();
+                    $("#divStep2").show();
+                    $("#divStep3").hide();
+                    $("#divStep4").hide();
+
+                    $("#stepRecord").val("1");
+                    $("#labelStepTitle").html(" 2: Adres");
+
+                    return;
+                }
+            },
+            "Volgende": function () {
+                if ($("#stepRecord").val() == "0") {
+                    $("#divStep1").hide();
+                    $("#divStep2").show();
+                    $("#divStep3").hide();
+                    $("#divStep4").hide();
+
+                    $("#stepRecord").val("1");
+
+                    $("#labelStepTitle").html(" 2: Adres");
+
+                    $(".ui-dialog-buttonset span:eq(1)").html('Finish');
+                    return;
+                }
+
+
+                if ($("#stepRecord").val() == "1") {
+                    $.gritter.add({
+                        // (string | mandatory) the heading of the notification
+                        title: 'Notification Center',
+                        // (string | mandatory) the text inside the notification
+                        text: 'U heeft alle stappen van de aanmaak van een persoon doorlopen. U zult automatisch naar het overzicht van uw bestaande personen worden geleid.',
+                        // (string | optional) the image to display on the left
+                        image: 'images/White_circle/document_blank.png',
+                        // (bool | optional) if you want it to fade out on its own or just sit there
+                        sticky: false,
+                        // (int | optional) the time you want it to be alive for before fading out
+                        time: '3000'
+                    });
+
+                    $(this).dialog('close');
+
+                    return false;
+                }
+            }
+        }
+    });
+
+    $("#btnCreate").button().click(function () {
+        $("#dialog-form").dialog("open");
+    });
+
+    $("#btnPrint").button();
+
+    $("#txtContractName").qtip({
+        content: {
+            attr: "title"
+        },
+        position: {
+            corner: {
+                target: 'rightMiddle',
+                tooltip: 'leftBottom'
+            }
+        },
+        show: {
+            when: {
+                event: 'focus'
+            }
+        },
+        hide: {
+            when: {
+                event: 'blur'
+            }
+        }
+    });
+
+    $("#divHelpContent").dialog({
+        title: 'HET Portaal -- Contract',
+        autoOpen: false,
+        height: 380,
+        width: 600,
+        modal: true
+    });
+});
---
+++ scripts/Person.js
@@ -0,0 +1,113 @@
+﻿var LastNames = ["Nancy", "Andrew", "Janet", "Margaret", "Steven", "Michael", "Robert", "Laura", "Anne", "Nige"],
+    FirstNames = ["Jackey", "Gary", "Ben"],
+    Nummers = ["0000000000", "0000000001", "0000000002", "0000000003", "0000000004", "0000000005", "0000000006", "0000000007", "0000000008", "0000000009"];
+
+function createPersonRandomData(count) {
+    var data = [];
+
+    for (var i = 0; i < count; i++) {
+        var lastname = LastNames[Math.floor(Math.random() * LastNames.length)],
+            firstname = FirstNames[Math.floor(Math.random() * FirstNames.length)],
+            nummer = Nummers[Math.floor(Math.random() * Nummers.length)];
+
+        data.push({
+            Id: i + 1,
+            LastName: lastname,
+            FirstName: firstname,
+            Nummer: nummer
+        });
+    }
+    return data;
+}
+
+var Achternaam = ["Jackey", "Dennis"],
+    Voornaam = ["Zhuang", "Klop"],
+    Functie = ["Implementatie consultant"],
+    Email = ["Jackey.Zhuang@thebeaglearmada.nl", "Dennis.Klop@thebeaglearmada.nl"],
+    Telephones = ["12345678", "87654321"];
+
+function createContactPersonRandomData(count) {
+    var data = [];
+
+    for (var i = 0; i < count; i++) {
+        var lastname = Achternaam[Math.floor(Math.random() * Achternaam.length)],
+            firstname = Voornaam[Math.floor(Math.random() * Voornaam.length)],
+            functie = Functie[Math.floor(Math.random() * Functie.length)],
+            email = Email[Math.floor(Math.random() * Email.length)],
+            telephone = Telephones[Math.floor(Math.random() * Telephones.length)];
+
+        data.push({
+            Id: i + 1,
+            LastName: lastname,
+            FirstName: firstname,
+            Functie: functie,
+            Email: email,
+            Telephone: telephone
+        });
+    }
+    return data;
+}
+
+var Adres = ["XXXX", "OOOO"],
+    Postbus = ["Postbus"],
+    Postcode = ["123456789"],
+    Plaats = ["minhang", "pudong"],
+    Land = ["China", "Netherlands"];
+
+function createAdresRandomData(count) {
+    var data = [];
+
+    for (var i = 0; i < count; i++) {
+        var address = Adres[Math.floor(Math.random() * Adres.length)],
+            postbus = Postbus[Math.floor(Math.random() * Postbus.length)],
+            postcode = Postcode[Math.floor(Math.random() * Postcode.length)],
+            place = Plaats[Math.floor(Math.random() * Plaats.length)],
+            country = Land[Math.floor(Math.random() * Land.length)];
+
+        data.push({
+            Id: i + 1,
+            Adres: address,
+            Postbus: postbus,
+            Postcode: postcode,
+            Plaats: place,
+            Land: country
+        });
+    }
+    return data;
+}
+
+function generatePeople(itemCount, callback) {
+    var data = [],
+        delay = 25,
+        interval = 500,
+        starttime;
+
+    var now = new Date();
+    setTimeout(function () {
+        starttime = +new Date();
+        do {
+            var firstName = firstNames[Math.floor(Math.random() * firstNames.length)],
+                lastName = lastNames[Math.floor(Math.random() * lastNames.length)],
+                city = cities[Math.floor(Math.random() * cities.length)],
+                title = titles[Math.floor(Math.random() * titles.length)],
+                birthDate = birthDates[Math.floor(Math.random() * birthDates.length)],
+                age = now.getFullYear() - birthDate.getFullYear();
+
+            data.push({
+                Id: data.length + 1,
+                FirstName: firstName,
+                LastName: lastName,
+                City: city,
+                Title: title,
+                BirthDate: birthDate,
+                Age: age
+            });
+        } while (data.length < itemCount && +new Date() - starttime < interval);
+
+        if (data.length < itemCount) {
+            setTimeout(arguments.callee, delay);
+        } else {
+            callback(data);
+        }
+    }, delay);
+}
